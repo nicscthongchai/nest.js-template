e2491f14d42d29667c0a1174a7b5795ff5e214ce
nicscthongchai <nicscthongchai@gmail.com>
2021-04-27 20:56:56 +0700

    update onStartListener

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import chalk from 'chalk'
+import { networkInterfaces } from 'os'
 import { AppConfig } from './configuration'
-
-const firstUppercase = (text: string) => {
-  return text[0].toUpperCase() + text.slice(1).toLowerCase()
-}
 
 @Injectable()
 export class AppService {
@@ -15,12 +12,25 @@ export class AppService {
 
   getOnStartListener() {
     const config = this.configService.get<AppConfig>(AppConfig.name)
-    const url = `http://localhost:${config.port}`
+    const network = networkInterfaces()
     return () => {
       console.log('')
       console.log(chalk.bold('Access URLs:'))
       console.log(chalk.gray('-------------------------------------'))
-      console.log(`${firstUppercase(config.env)}: ${chalk.magentaBright(url)}`)
+      console.log(
+        Object.keys(network)
+          .map((name) => ({
+            ...network[name].find((info) => info.family === 'IPv4'),
+            name,
+          }))
+          .map((info) => {
+            const { address } = info
+            const url = chalk.magentaBright(`http://${address}:${config.port}`)
+            return `${info.name}: ${url}`
+          })
+          .join('\n')
+          .replace('127.0.0.1', 'localhost'),
+      )
       console.log(chalk.gray('-------------------------------------'))
       console.log(chalk.blueBright(`Press ${chalk.italic('CTRL+C')} to stop`))
       console.log('')
